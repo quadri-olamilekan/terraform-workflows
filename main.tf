dabffd93e4ed0faad60ac53742a64f1fbe265203
quadribello <quadribello41@gmail.com>
2023-09-20 16:13:17 -0400

    create s3 bucket for backend

--- main.tf
+++ main.tf
@@ -1,12 +1,25 @@
 module "s3" {
-  source = "git@github.com:quadribello/Infra_Project.git//s3-module?ref=v1.0.2"
-  region = "us-east-1"
+  source = "git@github.com:quadribello/Infra_Project.git//s3-module?ref=v1.1.0"
   env    = "dev"
 }
 
-backend "s3" {
-  bucket = "bootcamp32-dev-47"
-  key    = "action/terraform.tfstate"
+#backend configuration
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  backend "s3" {
+    bucket  = "bootcamp32-dev-13"
+    region  = "us-east-1"
+    key     = "action/terraform.tfstate"
+    encrypt = true
+  }
+}
+
+provider "aws" {
   region = "us-east-1"
-  encrypt = true
 }
