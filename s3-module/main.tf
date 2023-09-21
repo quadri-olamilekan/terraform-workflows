c0612928dee4acb74e62172b0ffe4d85d68e71d5
quadribello <quadribello41@gmail.com>
2023-09-21 18:25:55 -0400

    test workflow

--- s3-module/main.tf
+++ s3-module/main.tf
@@ -13,7 +13,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket  = "bootcamp32-dev-13"
+    bucket  = "bootcamp32-dev-47"
     region  = "us-east-1"
     key     = "action/terraform.tfstate"
     encrypt = true
