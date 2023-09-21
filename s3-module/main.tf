a19354261e6f8c35115474b7bd5636e24d564239
quadribello <quadribello41@gmail.com>
2023-09-21 17:27:36 -0400

    test workflow

---
+++ s3-module/main.tf
@@ -0,0 +1,25 @@
+module "s3" {
+  source = "git@github.com:quadribello/Infra_Project.git//s3-module?ref=v1.2.0"
+  env    = "dev"
+}
+
+#backend configuration
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  backend "s3" {
+    bucket  = "bootcamp32-dev-13"
+    region  = "us-east-1"
+    key     = "action/terraform.tfstate"
+    encrypt = true
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
