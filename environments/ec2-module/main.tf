b53f3a228883231323a4a3d42622c27810404152
quadribello <quadribello41@gmail.com>
2023-09-22 13:46:15 -0400

    create ec2 and destroy resources

--- environments/ec2-module/main.tf
+++ environments/ec2-module/main.tf
@@ -1,6 +1,6 @@
 module "ec2_instance" {
-  #source = "git@github.com:quadribello/Infra_Project.git//s3-module?ref=v1.2.0"
-  source           = "git::https://github.com/quadribello/Infra_Project.git//ec2-module?ref=v1.3.3"
+  source = "git@github.com:quadribello/Infra_Project.git//ec2-module?ref=v1.3.3"
+  # source           = "git::https://github.com/quadribello/Infra_Project.git//ec2-module?ref=v1.3.3"
   region           = "us-east-1"
   instance_count   = 1
   my_instance_type = "t2.micro"
