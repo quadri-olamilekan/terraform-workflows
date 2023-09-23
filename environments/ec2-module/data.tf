ab663c9e6d2088598af920505f1e1bd79fa0683f
quadribello <quadribello41@gmail.com>
2023-09-22 22:03:48 -0400

    move vpc-module to infra_project

--- environments/ec2-module/data.tf
+++
@@ -1,8 +0,0 @@
-data "terraform_remote_state" "vpc" {
-  backend = "s3"
-  config = {
-    bucket = "bootcamp32-dev-47"
-    key    = "vpc/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
--- environments/ec2-module/main.tf
+++ environments/ec2-module/main.tf
@@ -1,10 +1,9 @@
 module "ec2_instance" {
-  source = "git@github.com:quadribello/Infra_Project.git//ec2-module?ref=v1.3.3"
+  source = "git@github.com:quadribello/Infra_Project.git//ec2-module?ref=v1.4.0"
   # source           = "git::https://github.com/quadribello/Infra_Project.git//ec2-module?ref=v1.3.3"
   region           = "us-east-1"
   instance_count   = 1
   my_instance_type = "t2.micro"
   name             = "dev_ec2"
-  vpc_id           = data.terraform_remote_state.vpc.outputs.vpc_id
   my_key           = "demo32"
 }
