865935e2f9794aaadd640321e466cea7861edb21
quadribello <quadribello41@gmail.com>
2023-09-22 22:31:07 -0400

    move vpc-module to infra_project

--- environments/ec2-module/main.tf
+++ environments/ec2-module/main.tf
@@ -3,6 +3,7 @@ module "ec2_instance" {
   # source           = "git::https://github.com/quadribello/Infra_Project.git//ec2-module?ref=v1.3.3"
   region           = "us-east-1"
   instance_count   = 1
+  vpc_id           = data.terraform_remote_state.vpc.outputs.vpc_id
   my_instance_type = "t2.micro"
   name             = "dev_ec2"
   my_key           = "demo32"
