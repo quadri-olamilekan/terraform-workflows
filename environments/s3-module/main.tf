4e9594ff16fc2f85920c8c152b2a07222577cd95
quadribello <quadribello41@gmail.com>
2024-01-06 13:58:31 -0500

    update repo

--- environments/s3-module/main.tf
+++ environments/s3-module/main.tf
@@ -1,6 +1,6 @@
 module "s3" {
-  #source = "git@github.com:quadribello/Infra_Project.git//s3-module?ref=v1.2.0"
-  source = "git::https://github.com/quadribello/Infra_Project.git//s3-module?ref=v1.2.0"
+  #source = "git@github.com:BellyBista/Infra_Project.git//s3-module?ref=v1.2.0"
+  source = "git::https://github.com/BellyBista/Infra_Project.git//s3-module?ref=v1.2.0"
   env    = "dev"
   region = "us-east-1"
 }
--- environments/vpc-module/main.tf
+++ environments/vpc-module/main.tf
@@ -1,4 +1,4 @@
 module "vpc" {
-  source = "git@github.com:quadribello/Infra_Project.git//vpc-module?ref=v1.4.2"
-  #source = "git::https://github.com/quadribello/Infra_Project.git//vpc-module?ref=v1.4.0"
+  source = "git@github.com:BellyBista/Infra_Project.git//vpc-module?ref=v1.4.2"
+  #source = "git::https://github.com/BellyBista/Infra_Project.git//vpc-module?ref=v1.4.0"
 }
